e9992ee231e344b91f5f73602537f30323a42952
Raul Barrios <barrau89@gmail.com>
2023-02-23 13:08:55 -0600

    DOM manipulation using querySelector (all commented out)

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 'use strict';
+/*
+// For the 'Guess Number' Section -------------------------- //
+console.log(document.querySelector('.message').textContent); //this is exactly like we'd select the same Element in CSS. The '.' is because it's a class, if it would be an ID then the '#' is needed.
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// For Correct Number Section -------------------------- //
+document.querySelector('.number').textContent = 13;
+
+// For the 'Score' Section -------------------------- //
+document.querySelector('.score').textContent = 10;
+
+// For the 'Input' Section -------------------------- //
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value); //with an input field, to get the actual value, we use the 'Value' property
+*/
